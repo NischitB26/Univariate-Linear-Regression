8afa0b3d8320be744742849bc93cc0778c512d13
NischitB26 <64847169+NischitB26@users.noreply.github.com>
2020-06-29 22:24:02 +0530

    Update linear_regression.py

--- linear_regression.py
+++ linear_regression.py
@@ -18,6 +18,7 @@ class LinearRegression:
             self.hyp_vec = X_train*self.theta1 + self.theta0
             self.J = sum((self.hyp_vec - y_train)**2)
             print('===== Cost : {} ====== Epoch : {} ====='.format(self.J, i+1))
+            # gradient descent
             self.d0 = self.frac * learning_rate * sum(self.hyp_vec - y_train)
             self.d1 = self.frac * learning_rate * np.dot((self.hyp_vec - y_train), X_train)
             self.theta0 -= self.d0
@@ -34,14 +35,17 @@ class LinearRegression:
 X = np.array([1.0, 2.0,  3.0,  4.0,  5.0,  6.0,  7.0,  8.0,  9.0, 10.0, 11.0, 12.0])
 y = np.array([5.0, 8.8,  9.6, 14.2, 18.8, 19.5, 21.4, 26.8, 28.9, 32.0, 33.8, 38.2])
 
+# model : y = m*x + b
+
 formula_m = (np.mean(X)*np.mean(y) - np.mean(X*y)) / (np.mean(X)**2 - np.mean(X**2))
 formula_b = np.mean(y) - formula_m*np.mean(X)
 formula_predictions = X*formula_m + formula_b
 
-model = LinearRegression()
+model = LinearRegression() 
 model.fit(standardize_data(X), y, learning_rate=0.05, epochs=2000)
 model_predictions = model.predict(standardize_data(X))
 
+# mean_squared_error
 print(sum((model_predictions - y)**2))
 print(sum((formula_predictions - y)**2))
 
